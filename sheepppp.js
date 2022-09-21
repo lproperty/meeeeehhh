85842fd19021f3829add4e21b7b0a6481de490a2
tess <liupeng511@gmail.com>
2022-09-21 12:33:32 +0800

    2022-09-21

--- sheepppp.js
+++ sheepppp.js
@@ -1,7 +1,9 @@
 /*
-[rewrite_local]
-^https?:\/\/cat-match\.easygame2021\.com\/sheep\/v1\/game\/map_info_ex\?matchType=.?$ url script-response-body https://raw.githubusercontent.com/harry-sunhao/PersonalRepo/main/Sheep_new.js
-[mitm]
+#!name=ylgy
+#!desc=羊了个羊
+[Script]
+Ylgy = type=http-response, pattern=^https?://cat-match.easygame2021.com/sheep/v1/game/map_info_ex(.*)$, requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/lproperty/meeeeehhh/master/sheepppp.js,argument=
+[MITM]
 hostname = cat-match.easygame2021.com
 */
 var obj = JSON.parse($response.body);

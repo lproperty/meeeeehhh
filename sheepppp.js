a16cc1e1d7f293bcd1cf64d7a73e9f6fe1eac771
tess <liupeng511@gmail.com>
2022-09-23 12:14:44 +0800

    Update sheepppp.js

--- sheepppp.js
+++ sheepppp.js
@@ -1,6 +1,6 @@
 /*
 #!name=ylgy
-#!desc=羊了个羊
+#!desc=yang
 [Script]
 Ylgy = type=http-response, pattern=^https?://cat-match.easygame2021.com/sheep/v1/game/map_info_ex(.*)$, requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/lproperty/meeeeehhh/master/sheepppp.js,argument=
 [MITM]

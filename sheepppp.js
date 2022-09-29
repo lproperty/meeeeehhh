4b9cc7b5f0803ce69ded57fdb25480473099efa1
tess <liupeng511@gmail.com>
2022-09-29 22:47:06 +0800

    Update sheepppp.js

--- sheepppp.js
+++ sheepppp.js
@@ -8,8 +8,8 @@ hostname = cat-match.easygame2021.com
 */
 var obj = JSON.parse($response.body);
 
-obj.data.map_md5[1] = "046ef1bab26e5b9bfe2473ded237b572";
+// obj.data.map_md5[1] = "046ef1bab26e5b9bfe2473ded237b572";
 
-// obj.data.map_seed = [0,0,0,0];
+obj.data.map_seed = [0,0,0,0];
 
 $done({body: JSON.stringify(obj)});

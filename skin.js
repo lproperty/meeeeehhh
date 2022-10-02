00cc1063e6b55ecdac9fd05cb48bb56d6e0083b5
tess <liupeng511@gmail.com>
2022-10-02 21:24:57 +0800

    Update and rename rank.js to skin.js

---
+++ skin.js
@@ -0,0 +1,13 @@
+/*
+#!name=topic
+#!desc=yang
+[Script]
+Ylgy = type=http-response, pattern=^https?://cat-match.easygame2021.com/sheep/v1/game/skin/info(.*)$, requires-body=1, max-size=0, script-path=https://raw.githubusercontent.com/lproperty/meeeeehhh/master/sheepppp.js,argument=
+[MITM]
+hostname = cat-match.easygame2021.com
+*/
+var obj = JSON.parse($response.body);
+
+obj.data.skin_list.push({"id" : 15});
+
+$done({body: JSON.stringify(obj)});
